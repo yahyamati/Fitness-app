8d8184408b3d410831bcecc09a3c52260165680b
Yahia-mati <yahyamati8@gmail.com>
2024-10-25 21:15:04 +0100

    some design

--- Fitness_front/src/Components/Exercise.jsx
+++ Fitness_front/src/Components/Exercise.jsx
@@ -16,16 +16,49 @@ const Exercise = () => {
     return <p className="text-center text-gray-400">Loading data...</p>;
   }
 
+  const exerciseData = [
+    {
+      image: assets.weight_lifting, 
+      title: "Weight Loss",
+      description: "Weight loss can have causes that aren't due to underlying disease. Examples include dieting, exercise, malnutrition or lack of access to food."
+    },
+    {
+      image: assets.yoga, 
+      title: "Classic Yoga",
+      description: 'The term "yoga" in the Western world often denotes a modern form of Hatha yoga and a posture-based physical fitness, stress-relief.'
+    },
+    {
+      image: assets.cyclisme, 
+      title: "Cycling",
+      description: "Cycling, also, when on a two-wheeled bicycle, called bicycling or biking, is the use of cycles for transport, recreation, exercise or sport."
+    },
+    {
+      image: assets.muscle, 
+      title: "Body Building",
+      description: "Bodybuilding is the use of progressive resistance exercise to control and develop one's muscles by muscle hypertrophy for aesthetic purposes."
+    },
+    {
+      image: assets.alter, 
+      title: "Musculation",
+      description: "Weight training is a common type of strength training for developing the strength, size of skeletal muscles and maintenance of strength."
+    },
+    {
+      image: assets.run, 
+      title: "Fitness Running",
+      description: "Weight training is a common type of strength training for developing the strength, size of skeletal muscles and maintenance of strength."
+    },
+  ];
+
   return (
-    <div className="pb-20 bg-black min-h-screen"> {/* Add padding for the navbar */}
-      <header className="relative h-[110vh] flex items-start justify-start">
+    <div className="pb-20 bg-black min-h-screen">
+      <header className="relative flex items-start justify-start h-[60vh] md:h-[110vh]">
         <img
           src={assets.gym}
           alt="Gym Background"
           className="absolute inset-0 object-cover w-full h-full z-0 opacity-50"
         />
-        <div className="relative z-10 text-left p-20">
-          <h1 className="text-7xl text-white mb-4 max-w-lg">
+        <div className="relative z-10 text-left p-20 md:p-20">
+          <h1 className="text-4xl md:text-7xl text-white mb-4 max-w-lg">
             Keep your body healthy & in shape
           </h1>
           <p className="text-gray-300 mb-10 text-lg max-w-md">
@@ -34,44 +67,19 @@ const Exercise = () => {
         </div>
       </header>
 
-      <div className="relative h-[120vh] flex items-center justify-center">
+      <div className="relative flex items-center justify-center h-auto">
         <img
           src={assets.gyminverse}
           alt="Gym Background"
           className="absolute inset-0 object-cover w-full h-full z-0 opacity-50"
         />
-
-        <div className="relative z-10 grid grid-cols-1 md:grid-cols-3 gap-4 p-10">
-          {[
-            {
-              title: "Weight Loss",
-              description: "Weight loss can have causes that aren't due to underlying disease. Examples include dieting, exercise, malnutrition or lack of access to food."
-            },
-            {
-              title: "Classic Yoga",
-              description: 'The term "yoga" in the Western world often denotes a modern form of Hatha yoga and a posture-based physical fitness, stress-relief.'
-            },
-            {
-              title: "Cycling",
-              description: "Cycling, also, when on a two-wheeled bicycle, called bicycling or biking, is the use of cycles for transport, recreation, exercise or sport."
-            },
-            {
-              title: "Body Building",
-              description: "Bodybuilding is the use of progressive resistance exercise to control and develop one's muscles by muscle hypertrophy for aesthetic purposes."
-            },
-            {
-              title: "Musculation",
-              description: "Weight training is a common type of strength training for developing the strength, size of skeletal muscles and maintenance of strength."
-            },
-            {
-              title: "Fitness Running",
-              description: "Weight training is a common type of strength training for developing the strength, size of skeletal muscles and maintenance of strength"
-            },
-          ].map((item, index) => (
-            <div key={index} className="flex flex-col">
-              <h2 className="text-white text-center pb-5 text-xl">{item.title}</h2>
-              <p className="text-gray-300">{item.description}</p>
-              {index <= 5 && <hr className="border-gray-600 my-4" />} 
+        <div className="relative z-10 grid grid-cols-1 md:grid-cols-3 gap-4 p-5 md:p-10">
+          {exerciseData.map((exercise, index) => (
+            <div key={index} className="flex flex-col items-center ">
+              <img src={exercise.image} alt={exercise.title} className="w-20 h-20 mb-2" />
+              <h2 className="text-white text-xl text-center pb-2">{exercise.title}</h2>
+              <p className="text-gray-300 text-center">{exercise.description}</p>
+              <hr className="w-full my-4 border-t border-gray-600" />
             </div>
           ))}
         </div>
@@ -83,9 +91,9 @@ const Exercise = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 px-5">
         {category.categories.map((cat) => (
           <Link
-            to={`/exercise/category/${cat.category}`} 
+            to={`/exercise/category/${cat.category}`}
             key={cat._id}
-            className="flex items-center p-4 bg-[#393939] rounded-lg shadow-md transition transform hover:scale-105 relative" // Make Link a relative container for absolute gradient
+            className="flex items-center p-4 bg-[#393939] rounded-lg shadow-md transition transform hover:scale-105 relative"
           >
             <img src={cat.image} alt={cat.category} className="w-20 h-20 mr-4" />
             <h2 className="text-2xl font-bold text-[#FFFFFF]">{cat.category}</h2>
--- Fitness_front/src/assets/assets.js
+++ Fitness_front/src/assets/assets.js
@@ -7,6 +7,12 @@ import profile_icon from './profile_icon.png'
 import logout_icon from './logout_icon.png'
 import gym from './gym.png'
 import gyminverse from './gym2.png'
+import alter from './alter.png'
+import yoga from './yoga.png'
+import cyclisme from './cyclisme.png'
+import muscle from './muscle.png'
+import weight_lifting from './weight_lifting.png'
+import run from './run.png'
 
 export const assets = {
     linkedin_icon,
@@ -16,6 +22,12 @@ export const assets = {
     profile_icon,
     logout_icon,
     gym,
-    gyminverse
+    gyminverse,
+    alter,
+    yoga,
+    cyclisme,
+    muscle,
+    weight_lifting,
+    run
 
 }
